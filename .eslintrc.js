15ece0b16ea09959b4d843de88c1f4d5b9888fb9
Alejandro Arango V <aalejo87@gmail.com>
2021-12-02 18:18:53 -0500

    Boilerplate
    Added ESLINT and latests dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,35 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
+  extends: [
+    '@react-native-community',
+    'airbnb-typescript',
+    'airbnb/hooks',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:jest/recommended',
+    'prettier',
+    'plugin:prettier/recommended',
+  ],
+  rules: {
+    'consistent-return': 0,
+    complexity: ['error', 12],
+    'import/no-extraneous-dependencies': 0,
+    'class-methods-use-this': 0,
+    'default-case': 0,
+    '@typescript-eslint/no-unused-vars': ['error', {args: 'none'}],
+    'react-native/no-inline-styles': 2,
+    'react-native/no-color-literals': 2,
+    'react-native/no-unused-styles': 2,
+    'react-native/no-single-element-style-arrays': 2,
+    'react/require-default-props': 0,
+    'react/prop-types': 0,
+    'react/jsx-props-no-spreading': 0,
+    'import/prefer-default-export': 0,
+  },
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+    project: [
+      './tsconfig.json',
+      './tsconfig.eslint.json',
+    ],
+  },
 };
